30977fc7deafe08ff320b725dd9fa6c295a4a694
Robert Hajek <hajek.rob@gmail.com>
2015-08-11 00:34:43 -0600

    finished exercise

--- js/main.js
+++ js/main.js
@@ -4,55 +4,70 @@ console.log("sanity check!");
 
 var students = [
   {
-   name: 'Derek',
-   age: 25,
-   city: 'Boulder'
+    name: 'Derek',
+    age: 25,
+    city: 'Boulder'
   },
   {
-   name: 'Liz',
-   age: 28,
-   city: 'Denver'
+    name: 'Liz',
+    age: 28,
+    city: 'Denver'
   },
   {
-   name: 'Carl',
-   age: 31,
-   city: 'Boulder'
+    name: 'Carl',
+    age: 31,
+    city: 'Boulder'
   },
   {
-   name: 'Peter',
-   age: 23,
-   city: 'Boulder'
+    name: 'Peter',
+    age: 23,
+    city: 'Boulder'
   },
   {
-   name: 'Megan',
-   age: 19,
-   city: 'Denver'
+    name: 'Megan',
+    age: 19,
+    city: 'Denver'
   }
 ];
+
 //sample
 function firstLoop(arr) {
   arr.forEach(function(student) {
     console.log(student.name);
   });
 }
-
 firstLoop(students);
 
 //problem 1
 function secondLoop(arr) {
-  arr.forEach(function(age) {
+  arr.forEach(function(student) {
     console.log(student.age);
   });
 }
+secondLoop(students);
 
 //problem 2
 function thirdLoop(arr) {
-  arr.forEach(function(name, city) {
-    console.log(student.name, student.city);
+  arr.forEach(function(student) {
+    console.log(students.name, students.city);
   });
 }
+thirdLoop(students);
 
-//problem 3
-// function fourthLoop(arr) {
-//   arr
-// }
+// problem 3
+function fourthLoop(arr) {
+  for (i=0;i<3;i++){
+    console.log(students.name + " is from " + students.city);
+  }
+}
+fourthLoop(students);
+
+//problem 4
+function fifthLoop(arr) {
+  arr.forEach(function(student) {
+    if (student.age > 25) {
+      console.log(student.name + " is older than 25");
+    }
+  });
+}
+fifthLoop(students);
